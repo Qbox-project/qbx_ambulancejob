2ceda842a0ff4d5975ca2f706743a779054d85f5
Manason <clanerp@gmail.com>
2023-07-29 17:01:00 -0700

    feat: allow respawn migration

--- client/load-unload.lua
+++ client/load-unload.lua
@@ -15,7 +15,7 @@ local function initDeathAndLastStand(metadata)
     if not metadata.inlaststand and metadata.isdead then
         exports['qbx-medical']:setDeathTime(Laststand.ReviveInterval)
         exports['qbx-medical']:killPlayer()
-        exports['qbx-medical']:AllowRespawn(IsInHospitalBed)
+        exports['qbx-medical']:AllowRespawn()
     elseif metadata.inlaststand and not metadata.isdead then
         StartLastStand()
     else
