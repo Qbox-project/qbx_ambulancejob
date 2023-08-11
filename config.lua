3ed8cc0308c688dfc6cfd641a436ddd41ef2aa9d
Manason <clanerp@gmail.com>
2023-08-11 10:03:16 -0700

    refactor: bed overhaul (#78)
    * refactor: bed overhaul
    
    * prevent non-inmates from respawning in jail hospital
    
    ---------
    
    Co-authored-by: ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>

--- config.lua
+++ config.lua
@@ -9,10 +9,6 @@ Config.AIHealTimer = 20 -- How long it will take to be healed after checking in,
 Config.LaststandMinimumRevive = 300
 
 Config.Locations = { -- Edit the various interaction points for players or create new ones
-    checking = {
-        [1] = vec3(308.19, -595.35, 43.29),
-        [2] = vec3(-254.54, 6331.78, 32.43),
-    },
     duty = {
         [1] = vec3(311.18, -599.25, 43.29),
         [2] = vec3(-254.88, 6324.5, 32.58),
@@ -38,32 +34,47 @@ Config.Locations = { -- Edit the various interaction points for players or creat
     stash = {
         [1] = vec3(309.78, -596.6, 43.29),
     },
+
     ---@class Bed
     ---@field coords vector4
-    ---@field taken boolean
     ---@field model number
 
-    ---@type Bed[]
-    beds = {
-        [1] = { coords = vec4(353.1, -584.6, 43.11, 152.08), taken = false, model = 1631638868 },
-        [2] = { coords = vec4(356.79, -585.86, 43.11, 152.08), taken = false, model = 1631638868 },
-        [3] = { coords = vec4(354.12, -593.12, 43.1, 336.32), taken = false, model = 2117668672 },
-        [4] = { coords = vec4(350.79, -591.8, 43.1, 336.32), taken = false, model = 2117668672 },
-        [5] = { coords = vec4(346.99, -590.48, 43.1, 336.32), taken = false, model = 2117668672 },
-        [6] = { coords = vec4(360.32, -587.19, 43.02, 152.08), taken = false, model = -1091386327 },
-        [7] = { coords = vec4(349.82, -583.33, 43.02, 152.08), taken = false, model = -1091386327 },
-        [8] = { coords = vec4(326.98, -576.17, 43.02, 152.08), taken = false, model = -1091386327 },
-        --- paleto
-        [9] = { coords = vec4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672 },
-        [10] = { coords = vec4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672 },
-        [11] = { coords = vec4(-255.98, 6315.67, 32.34, 313.91), taken = false, model = 2117668672 },
+    ---@type table<string, {coords: vector3, checkIn?: vector3, beds: Bed[]}>
+    hospitals = {
+        pillbox = {
+            coords = vec3(350, -580, 43),
+            checkIn = vec3(308.19, -595.35, 43.29),
+            beds = {
+                { coords = vec4(353.1, -584.6, 43.11, 152.08), model = 1631638868 },
+                { coords = vec4(356.79, -585.86, 43.11, 152.08), model = 1631638868 },
+                { coords = vec4(354.12, -593.12, 43.1, 336.32), model = 2117668672 },
+                { coords = vec4(350.79, -591.8, 43.1, 336.32), model = 2117668672 },
+                { coords = vec4(346.99, -590.48, 43.1, 336.32), model = 2117668672 },
+                { coords = vec4(360.32, -587.19, 43.02, 152.08), model = -1091386327 },
+                { coords = vec4(349.82, -583.33, 43.02, 152.08), model = -1091386327 },
+                { coords = vec4(326.98, -576.17, 43.02, 152.08), model = -1091386327 },
+            }
+        },
+        paleto = {
+            coords = vec3(-250, 6315, 32),
+            checkIn = vec3(-254.54, 6331.78, 32.43),
+            beds = {
+                { coords = vec4(-252.43, 6312.25, 32.34, 313.48), model = 2117668672 },
+                { coords = vec4(-247.04, 6317.95, 32.34, 134.64), model = 2117668672 },
+                { coords = vec4(-255.98, 6315.67, 32.34, 313.91), model = 2117668672 },
+            }
+        },
+        jail = {
+            coords = vec3(1761, 2600, 46),
+            beds = {
+                { coords = vec4(1761.96, 2597.74, 45.66, 270.14), model = 2117668672 },
+                { coords = vec4(1761.96, 2591.51, 45.66, 269.8), model = 2117668672 },
+                { coords = vec4(1771.8, 2598.02, 45.66, 89.05), model = 2117668672 },
+                { coords = vec4(1771.85, 2591.85, 45.66, 91.51), model = 2117668672 },
+            }
+        }
     },
-    jailbeds = {
-        [1] = { coords = vec4(1761.96, 2597.74, 45.66, 270.14), taken = false, model = 2117668672 },
-        [2] = { coords = vec4(1761.96, 2591.51, 45.66, 269.8), taken = false, model = 2117668672 },
-        [3] = { coords = vec4(1771.8, 2598.02, 45.66, 89.05), taken = false, model = 2117668672 },
-        [4] = { coords = vec4(1771.85, 2591.85, 45.66, 91.51), taken = false, model = 2117668672 },
-    },
+
     stations = {
         [1] = { label = Lang:t('info.pb_hospital'), coords = vec4(304.27, -600.33, 43.28, 272.249) }
     }
