9b5674de5e0d8ef6d7fcba68fd436cd959ba509b
Stan <96954031+tom-osborne@users.noreply.github.com>
2023-09-29 10:53:35 +0100

    chore(locales): Remove unused locales and add missing locales (english)

--- locales/en.lua
+++ locales/en.lua
@@ -1,31 +1,25 @@
 local Translations = {
     error = {
         canceled = 'Canceled',
-        bled_out = 'You have bled out...',
         impossible = 'Action Impossible...',
         no_player = 'No Player Nearby',
         no_firstaid = 'You need a First Aid Kit',
         no_bandage = 'You need a Bandage',
         beds_taken = 'Beds are occupied...',
         possessions_taken = 'All your possessions have been taken...',
-        not_enough_money = 'You don\'t have enough money on you...',
         cant_help = 'You can\'t help this person...',
         not_ems = 'You are not EMS or not signed in',
-        not_online = 'Player Not Online'
     },
     success = {
         revived = 'You revived a person',
         healthy_player = 'Player is Healthy',
         helped_player = 'You helped the person',
-        wounds_healed = 'Your wounds have been healed!',
         being_helped = 'You are being helped...'
     },
     info = {
         civ_died = 'Civilian Died',
         civ_down = 'Civilian Down',
         civ_call = 'Civilian Call',
-        self_death = 'Themselves or an NPC',
-        wep_unknown = 'Unknown',
         respawn_txt = 'RESPAWN IN: ~r~%{deathtime}~s~ SECONDS',
         respawn_revive = 'HOLD [~r~E~s~] FOR %{holdtime} SECONDS TO RESPAWN FOR $~r~%{cost}~s~',
         bleed_out = 'YOU WILL BLEED OUT IN: ~r~%{time}~s~ SECONDS',
@@ -39,9 +33,6 @@ local Translations = {
         healthy = 'You are completely healthy again!',
         safe = 'Hospital Safe',
         pb_hospital = 'Pillbox Hospital',
-        pain_message = 'Your %{limb} feels %{severity}',
-        many_places = 'You have pain in many places...',
-        bleed_alert = 'You are %{bleedstate}',
         ems_alert = 'EMS Alert - %{text}',
         mr = 'Mr.',
         mrs = 'Mrs.',
@@ -52,31 +43,15 @@ local Translations = {
         check_health = 'Check a Players Health',
         heal_player = 'Heal a Player',
         revive_player = 'Revive a Player',
-        revive_player_a = 'Revive A Player or Yourself (Admin Only)',
-        player_id = 'Player ID (may be empty)',
-        pain_level = 'Set Yours or A Players Pain Level (Admin Only)',
-        kill = 'Kill A Player or Yourself (Admin Only)',
-        heal_player_a = 'Heal A Player or Yourself (Admin Only)',
     },
     mail = {
         sender = 'Pillbox Hospital',
         subject = 'Hospital Costs',
         message = 'Dear %{gender} %{lastname}, <br /><br />Hereby you received an email with the costs of the last hospital visit.<br />The final costs have become: <strong>$%{costs}</strong><br /><br />We wish you a quick recovery!'
     },
-    states = {
-        irritated = 'irritated',
-        quite_painful = 'quite painful',
-        painful = 'painful',
-        really_painful = 'really painful',
-        little_bleed = 'bleeding a little bit...',
-        bleed = 'bleeding...',
-        lot_bleed = 'bleeding a lot...',
-        big_bleed = 'bleeding very much...',
-    },
     menu = {
         amb_vehicles = 'Ambulance Vehicles',
         status = 'Health Status',
-        close = '⬅ Close Menu',
     },
     text = {
         pstash_button = '[E] - Personal stash',
@@ -87,7 +62,6 @@ local Translations = {
         armory_button = '[E] - Armory',
         armory = 'Armory',
         veh_button = '[E] - Grab / Store Vehicle',
-        heli_button = '[E] - Grab / Store Helicopter',
         elevator_roof = '[E] - Take the elevator to the roof',
         elevator_main = '[E] - Take the elevator down',
         el_roof = 'Take the elevator to the roof',
@@ -99,24 +73,8 @@ local Translations = {
         check = 'Check In',
         lie_bed = '[E] - To lay in bed',
         bed = 'Lay in bed',
+        bed_out = 'Get out of bed',
         alert = 'Alert!'
-    },
-    body = {
-        head = 'Head',
-        neck = 'Neck',
-        spine = 'Spine',
-        upper_body = 'Upper Body',
-        lower_body = 'Lower Body',
-        left_arm = 'Left Arm',
-        left_hand = 'Left Hand',
-        left_fingers = 'Left Fingers',
-        left_leg = 'Left Leg',
-        left_foot = 'Left Foot',
-        right_arm = 'Right Arm',
-        right_hand = 'Right Hand',
-        right_fingers = 'Right Fingers',
-        right_leg = 'Right Leg',
-        right_foot = 'Right Foot',
     },
     progress = {
         ifaks = 'Taking ifaks...',
@@ -125,10 +83,6 @@ local Translations = {
         revive = 'Reviving Person...',
         healing = 'Healing Wounds...',
         checking_in = 'Checking in...',
-    },
-    logs = {
-        death_log_title = "%{playername} (%{playerid}) is dead",
-        death_log_message = "%{killername} has killed %{playername} with a **%{weaponlabel}** (%{weaponname})",
     }
 }
 
--- locales/it.lua
+++ locales/it.lua
@@ -1,31 +1,25 @@
 local Translations = {
     error = {
         canceled = 'Annullato',
-        bled_out = 'Sei dissanguato...',
         impossible = 'Azione impossibile...',
         no_player = 'Nessun giocatore nelle vicinanze',
         no_firstaid = 'Hai bisogno di un kit di pronto soccorso',
         no_bandage = 'Hai bisogno di una benda',
         beds_taken = 'I letti sono occupati...',
         possessions_taken = 'Hai perso i tuoi effetti personali...',
-        not_enough_money = 'Non hai abbastanza soldi...',
         cant_help = 'Non puoi aiutare questa persona...',
         not_ems = 'Non sei EMS o non sei in servizio',
-        not_online = 'Giocatore offline'
     },
     success = {
         revived = 'Hai rianimato una persona',
         healthy_player = 'Il giocatore è in salute',
         helped_player = 'Hai aiutato una persona',
-        wounds_healed = 'Le tue ferite sono state guarite!',
         being_helped = 'Ti stanno aiutando...'
     },
     info = {
         civ_died = 'Civile morto',
         civ_down = 'Civile ferito',
         civ_call = 'Richiesta di intervento',
-        self_death = 'Se stesso o un NPC',
-        wep_unknown = 'Sconosciuto',
         respawn_txt = 'RESPAWN TRA: ~r~%{deathtime}~s~ SECONDI',
         respawn_revive = 'PREMI [~r~E~s~] PER %{holdtime} SECONDI PER IL RESPAWN AL COSTO DI $~r~%{cost}~s~',
         bleed_out = 'MORIRAI DISSANGUATO TRA: ~r~%{time}~s~ SECONDI',
@@ -39,9 +33,6 @@ local Translations = {
         healthy = 'Sei di completamente in salute!',
         safe = 'Hospital Safe',
         pb_hospital = 'Pillbox Hospital',
-        pain_message = 'Hai %{limb} e ti senti %{severity}',
-        many_places = 'Hai dolori in molti posti...',
-        bleed_alert = 'Sei %{bleedstate}',
         ems_alert = 'EMS Alert - %{text}',
         mr = 'Sig.',
         mrs = 'Sig.ra',
@@ -51,31 +42,15 @@ local Translations = {
         check_health = 'Controlla la salute di un giocatore',
         heal_player = 'Guarisci giocatore',
         revive_player = 'Rianima giocatore',
-        revive_player_a = 'Rianima un giocatore o te stesso (Solo Admin)',
-        player_id = 'ID Giocatore (può essere vuoto)',
-        pain_level = 'Imposta il tuo o un livello di dolore di un giocatore (Solo Admin)',
-        kill = 'Uccidi un giocatore o te stesso (Solo Admin)',
-        heal_player_a = 'Cura un giocatore o te stesso (Solo Admin)',
     },
     mail = {
         sender = 'Pillbox Hospital',
         subject = 'Spese ospedaliere',
         message = 'Salve %{gender} %{lastname}, <br /><br />Con la presente hai ricevuto un\'e-mail con i costi dell\'ultima visita in ospedale.<br />I costi totali sono: <strong>$%{costs}</strong><br /><br />Le auguriamo una pronta guarigione!'
     },
-    states = {
-        irritated = 'irritato',
-        quite_painful = 'abbastanza doloroso',
-        painful = 'doloroso',
-        really_painful = 'davvero doloroso',
-        little_bleed = 'sanguindo un po\'...',
-        bleed = 'sanguindo...',
-        lot_bleed = 'sanguindo molto...',
-        big_bleed = 'sanguindo tantissimo...',
-    },
     menu = {
         amb_vehicles = 'Veicoli EMS',
         status = 'Stato',
-        close = '⬅ Chiudi Menù',
     },
     text = {
         pstash_button = '[E] - Inventario Personale',
@@ -86,32 +61,19 @@ local Translations = {
         armory_button = '[E] - Farmacia',
         armory = 'Farmacia',
         veh_button = '[E] - Prendi / Deposita Veicolo',
-        heli_button = '[E] - Prendi / Deposita Velivolo',
         elevator_roof = '[E] - Prendi l\'ascensore fino al tetto',
         elevator_main = '[E] - Prendi l\'ascensore fino al piano',
+        el_roof = 'Take the elevator to the roof',
+        el_main = 'Take the elevator to the main floor',
         bed_out = '[E] - Per alzarti dal letto..',
         call_doc = '[E] - Chiama un dottore',
         call = 'Chiama',
         check_in = '[E] Check in',
         check = 'Check In',
-        lie_bed = '[E] - Sdraiarsi sul letto'
-    },
-    body = {
-        head = 'Testa',
-        neck = 'Collo',
-        spine = 'Dorso',
-        upper_body = 'Torace',
-        lower_body = 'Bacino',
-        left_arm = 'Braccio sinistro',
-        left_hand = 'Mano sinistra',
-        left_fingers = 'Dita sinistra',
-        left_leg = 'Gamba sinistra',
-        left_foot = 'Piede sinistro',
-        right_arm = 'Braccio destro',
-        right_hand = 'Mano destra',
-        right_fingers = 'Dita destra',
-        right_leg = 'Gamba destra',
-        right_foot = 'Piede destro',
+        lie_bed = '[E] - Sdraiarsi sul letto',
+        bed = 'Lay in bed',
+        bed_out = 'Get out of bed',
+        alert = 'Alert!'
     },
     progress = {
         ifaks = 'Assumendo iFaks...',
@@ -120,10 +82,6 @@ local Translations = {
         revive = 'Rianimazione...',
         healing = 'Guarendo ferite...',
         checking_in = 'Check-in...',
-    },
-    logs = {
-        death_log_title = "%{playername} (%{playerid}) è morto",
-        death_log_message = "%{killername} ha ucciso %{playername} con **%{weaponlabel}** (%{weaponname})",
     }
 }
 
