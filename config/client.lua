df2e653d33919a26e9daff558bcc4935c0244ba5
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-24 22:05:09 -0500

    fix: missing config option

--- config/client.lua
+++ config/client.lua
@@ -4,6 +4,7 @@ return {
     painkillerInterval = 60, -- Time in minutes that painkillers last for
     checkInHealTime = 20, -- Time in seconds that it takes to be healed from the check-in system
     laststandTimer = 300, -- Time in seconds that the laststand timer lasts
+    aiHealTimer = 20, -- How long it will take to be healed after checking in, in seconds
 
     ---@alias Grade integer job grade
     ---@alias VehicleName string as appears in QBCore shared config
