b64edcee078955c47efff83451728d3c510ad5c4
Solareon <769465+solareon@users.noreply.github.com>
2023-11-29 06:13:22 +0100

    refactor: enable debugPoly via config
    * refactor: enable debugPoly via config
    
    * missed one debug
    
    * job.lua debugPoly

--- config/server.lua
+++ config/server.lua
@@ -1,4 +1,4 @@
 return {
     doctorCallCooldown = 1, -- Time in minutes for cooldown between doctors calls
-    wipeInvOnRespawn = true, -- Enable ro disable removing all items from player on respawn
+    wipeInvOnRespawn = true, -- Enable to disable removing all items from player on respawn
 }
