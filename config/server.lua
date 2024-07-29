58ed0c4ef1ed14ccde45f47c4be863379ec8f1eb
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-07-28 21:49:36 -0400

    chore: clean slate

--- config/server.lua
+++ config/server.lua
@@ -1,7 +1,1 @@
-return {
-    doctorCallCooldown = 1, -- Time in minutes for cooldown between doctors calls
-    wipeInvOnRespawn = true, -- Enable to disable removing all items from player on respawn
-    depositSociety = function(society, amount)
-        exports['Renewed-Banking']:addAccountMoney(society, amount)
-    end
-}
+return {}
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,37 +1,32 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qbx_ambulancejob'
+name 'qbx_ambulancejob'
+description 'EMS System for Qbox'
 repository 'https://github.com/Qbox-project/qbx_ambulancejob'
 version '1.0.0'
 
 ox_lib 'locale'
 
-dependencies {
-    'qbx_core',
-	'qbx_medical',
-    'ox_lib',
-	'ox_inventory'
-}
-
 shared_scripts {
-	'@ox_lib/init.lua',
-	'@qbx_core/modules/lib.lua',
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/lib.lua'
 }
 
 client_scripts {
-	'@qbx_core/modules/playerdata.lua',
-	'client/*.lua',
+    '@qbx_core/modules/playerdata.lua',
+    'client/main.lua'
 }
 
 server_scripts {
-	'server/*.lua',
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
 }
 
 files {
-	'locales/*.json',
-	'config/client.lua',
-	'config/shared.lua',
+    'config/client.lua',
+    'config/shared.lua',
+    'client/vehicles.lua',
 }
 
 lua54 'yes'
